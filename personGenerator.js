00ea1958215ae991f4ff9da2b2d7fe4f45222646
Dmitrousov <artem.dmitrousov@yandex.ru>
2024-07-22 22:51:31 +0500

    fixed personGenerator.js

--- personGenerator.js
+++ personGenerator.js
@@ -81,21 +81,21 @@ const personGenerator = {
         const prop = `id_${this.randomIntNumber(obj.count, 1)}`;  // this = personGenerator
         return obj.list[prop];
     },
-
-    randomBirthDay: function (){
-        let birthDay = 1;
-        if (this.personBirthMonth == "февраля") {
-            birthDay = Math.floor(Math.random() * (28 - 1) + 1);
-        } else if (this.personBirthMonth == "апреля" || this.personBirthMonth == "июня" || this.personBirthMonth == "сентября" || this.personBirthMonth == "ноября") {
-            birthDay = Math.floor(Math.random() * (30 - 1) + 1);
-        } else if (this.personBirthMonth == "января" || this.personBirthMonth == "марта" || this.personBirthMonth == "мая" || this.personBirthMonth == "июля" || this.personBirthMonth == "августа" || this.personBirthMonth == "октября" || this.personBirthMonth == "декабря") {
-            birthDay = Math.floor(Math.random() * (31 - 1) + 1);
-        }
-        return birthDay;
-    },
-
+       
     randomBirthMonth: function() {
         return this.randomValue(this.birthMonthJson);
+    },
+
+    randomBirthDay: function (){
+        let birthDay = Math.floor(Math.random() * 28 + 1);
+        if (this.randomBirthMonth == "февраля") {
+            return birthDay;
+        } else if (this.randomBirthMonth == "апреля" || this.personBirthMonth == "июня" || this.personBirthMonth == "сентября" || this.personBirthMonth == "ноября") {
+            birthDay = Math.floor(Math.random() * 30 + 1);
+        } else if (this.randomBirthMonth == "января" || this.personBirthMonth == "марта" || this.personBirthMonth == "мая" || this.personBirthMonth == "июля" || this.personBirthMonth == "августа" || this.personBirthMonth == "октября" || this.personBirthMonth == "декабря") {
+            birthDay = Math.floor(Math.random() * 31 + 1);
+        }
+        return birthDay;       
     },
 
     randomGender: function(){
