fa66a8f6d2ee8cc711d0c6aee9f7e3e2c86f9f95
Dmitrousov <artem.dmitrousov@yandex.ru>
2024-07-23 00:04:19 +0500

    added event listener

--- init.js
+++ init.js
@@ -12,6 +12,21 @@ window.onload = function()
     document.getElementById('birthYearOutput').innerText = initPerson.birthYear + ' года рождения';
 };
 
+	document.getElementById('generate').addEventListener('click', 
+		(event) => {
+		const initPerson = personGenerator.getPerson();
+    	document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    	document.getElementById('middleNameOutput').innerText = initPerson.middleName;
+    	document.getElementById('surnameOutput').innerText = initPerson.surname;
+    	document.getElementById('professionOutput').innerText = initPerson.profession;
+    	document.getElementById('genderOutput').innerText = initPerson.gender;
+    	document.getElementById('birthDayOutput').innerText = initPerson.birthDay;
+    	document.getElementById('birthMonthOutput').innerText = initPerson.birthMonth;
+    	document.getElementById('birthYearOutput').innerText = initPerson.birthYear + ' года рождения';
+		}
+
+	);
+
 	document.getElementById('default').addEventListener('click', 
 		(event) => {
 		document.getElementById('firstNameOutput').innerText = 'Иван';
